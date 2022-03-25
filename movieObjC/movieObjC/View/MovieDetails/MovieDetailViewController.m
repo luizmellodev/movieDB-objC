8e6e53cb77e9696214e0904d3ed5701875d54c4e
Luiz Eduardo <luizdudumr@gmail.com>
2022-03-25 17:34:50 -0300

    DetailView done with Overview API working :fire:

--- movieObjC/movieObjC/View/MovieDetails/MovieDetailViewController.m
+++ movieObjC/movieObjC/View/MovieDetails/MovieDetailViewController.m
@@ -29,16 +29,47 @@
     return 2;
 }
 
+- (void)fetchMovieData {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat: @"https://api.themoviedb.org/3/movie/%@/?api_key=06ba203a54577bd8c91c3b4d4b14d0ec&language=en-US",self.movie.identifier]];
+
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        NSError *err;
+        NSDictionary *movieListPageJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingFragmentsAllowed error:&err];
+        if (err) {
+            NSLog(@"Failed to serialize into JSON: %@", err);
+            return;
+        }
+
+        NSArray *movieList = movieListPageJSON[@"results"];
+
+        for (NSDictionary *movie in movieList) {
+            TheMovie *themovieDetails = TheMovie.new;
+            themovieDetails.identifier = movie[@"id"];
+            themovieDetails.overview = movie[@"overview"];
+
+
+//            [movies addObject: themovieDetails];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.detailTableView reloadData];
+        });
+
+    }] resume];
+}
+
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.row == 0) {
         MovieDetailTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"moviedetail" forIndexPath:indexPath];
         
-        NSString *testString = @"Teste de genero";
+//        NSString *testString = @"Teste de genero";
         
         cell.movieTitle.text = self.movie.title;
         cell.voteAverage.text = self.movie.voteAverage.stringValue;
-        cell.movieGenres.text = testString;
+//        cell.movieGenres.text = testString;
         
         // MARK:: separar em outra classe (?)
         NSString *urlPrefix = @"https://image.tmdb.org/t/p/w500";
@@ -53,7 +84,7 @@
         OverviewTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"moviedetailoverview" forIndexPath:indexPath];
         NSString *testString2 = @"Teste de generoTeste de generoTeste de generoTeste de generoTeste de generoTeste de generoTeste de genero";
 
-        cell.OverviewText.text =testString2;
+        cell.OverviewText.text =self.movie.overview;
         
         return cell;
 }
